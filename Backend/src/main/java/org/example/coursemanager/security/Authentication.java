59602e492a35d5466b0165629ceac80d6603bade
Kiss Ákos <kissakos2002@mailbox.unideb.hu>
2025-09-16 17:37:27 +0200

    "Added Bearer header for login response"

--- Backend/src/main/java/org/example/coursemanager/security/Authentication.java
+++ Backend/src/main/java/org/example/coursemanager/security/Authentication.java
@@ -6,6 +6,9 @@ import org.example.coursemanager.model.Roles;
 import org.example.coursemanager.model.User;
 import org.example.coursemanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -44,15 +47,18 @@ public class Authentication {
     }
 
     @PostMapping("/auth/login")
-    public String login(@RequestBody User loginRequest) {
+    public ResponseEntity<String> login(@RequestBody User loginRequest) {
         org.springframework.security.core.Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 
         if (authentication.isAuthenticated()){
             CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-            return jwtService.generateToken(userDetails.getEmail());
+            String token = jwtService.generateToken(userDetails.getEmail());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.AUTHORIZATION,"Bearer "+token)
+                    .body("Logged in!");
         }
         else{
-            return "Incorrect username/password";
+            return new ResponseEntity<>("Bad username/password!", HttpStatus.BAD_REQUEST);
         }
     }
 
